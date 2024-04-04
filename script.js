e8460f51b02b1b70cf1da8f14176cde98e5bbafd
Roxana <ileanaroxanamitroi@gmail.com>
2024-04-05 02:32:07 +0300

    Add basic UI

--- script.js
+++ script.js
@@ -1,52 +1,47 @@
-playGame();
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const results = document.querySelector("div");
 
+rock.addEventListener("click", () => {
+    const computerSelection = getComputerSelection();
+    if (computerSelection === "rock")
+        results.textContent = "It's a draw!";
+    else if (computerSelection === "paper")
+        results.textContent = "You Lose! Paper beats Rock";
+    else if (computerSelection === "scissors")
+        results.textContent = "You Win! Rock beats Scissors";
+});
 
-function computerSelection() {
+paper.addEventListener("click", () => {
+    const computerSelection = getComputerSelection();
+    if (computerSelection === "paper")
+        results.textContent = "It's a draw!";
+    else if (computerSelection === "scissors")
+        results.textContent = "You Lose! Scissors beats Paper";
+    else if (computerSelection === "rock")
+        results.textContent = "You Win! Paper beats Rock";
+});
+
+scissors.addEventListener("click", () => {
+    const computerSelection = getComputerSelection();
+    if (computerSelection === "scissors")
+        results.textContent = "It's a draw!";
+    else if (computerSelection === "paper")
+        results.textContent = "You Win! Scissors beats Paper";
+    else if (computerSelection === "rock")
+        results.textContent = "You Lose! Rock beats Scissors";
+});
+
+function getComputerSelection() {
     let rand = Math.floor(Math.random() * 3);
 
-    if (rand == 0) 
-        return "rock";
-    if (rand == 1) 
-        return "paper";
-    if (rand == 2) 
-        return "scissors";
-}
-
-function playerSelection() {
-    let choice;
-    do {
-        choice = prompt("Choice?", "");
-        choice = choice.toLowerCase();
-    } while (choice !== "rock" && choice !== "paper" && choice !== "scissors");
-    
-    return choice;
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection)
-        console.log("It's a draw!");
-    
-    else if (playerSelection === "rock" && computerSelection === "paper")
-        console.log("You Win! Paper beats Rock");
-    else if (computerSelection === "rock" && playerSelection === "paper")
-        console.log("You Lose! Paper beats Rock");
-    
-    else if (playerSelection === "rock" && computerSelection === "scissors")
-        console.log("You Win! Rock beats Scissors");
-    else if (computerSelection === "rock" && playerSelection === "scissors")
-        console.log("You Lose! Rock beats Scissors");
-    
-    else if (playerSelection === "scissors" && computerSelection === "paper")
-        console.log("You Win! Scissors beats Paper");
-    else if (computerSelection === "scissors" && playerSelection === "paper")
-        console.log("You Lose! Scissors beats Paper");    
-    
-    // rock beats scissors    
-    // paper beats rock      
-    // scissors beats paper   
+    switch (rand) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
     }
-    
-function playGame() {
-    for (let i = 0; i < 5; i++)
-        playRound(playerSelection(), computerSelection());
 }
